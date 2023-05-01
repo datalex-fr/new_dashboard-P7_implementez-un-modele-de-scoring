4edfb0c525851b6fd5a7ddb3bcc80abb0859ee96
datalex-fr <102934323+datalex-fr@users.noreply.github.com>
2023-05-01 05:45:09 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -155,8 +155,8 @@ def main():
         #conversion en pd.DataFrame et pd.Series
         data_neig = pd.DataFrame(content['data_neigh'])
         target_neig = (pd.Series(content['y_neigh']).rename('TARGET'))
-        x_custo = pd.DataFrame(content['x_client'])
-        return data_neig, target_neig, x_custo
+        x_customer = pd.DataFrame(content['x_client'])
+        return data_neig, target_neig, x_customer
 
 
 
